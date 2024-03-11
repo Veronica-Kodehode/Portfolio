4aa5f03a7acfb7784cb570d9d2016ebd306844ad
unknown <veronicas.kodehode@gmail.com>
2024-03-11 23:23:26 +0100

    general polish + added project-data

--- script.js
+++ script.js
@@ -4,18 +4,10 @@ const projects = document.getElementById("projects");
 const homeBtn = document.getElementById("home-btn");
 const aboutBtn = document.getElementById("about-btn");
 const projectsBtn = document.getElementById("projects-btn");
-const projectCard = document.querySelectorAll(".project-card");
-// const displayProjects = document.getElementById("display-projects");
-const navBar = document.getElementById("nav-bar");
-const projectOne = document.getElementById("project-one");
 const projectContainer = document.getElementById("project-container");
-const contactBtn = document.getElementById("contact-btn");
 const creativity = document.getElementById("creativity");
-const navBtn = document.getElementById("nav-btn");
 
-// when clicking between the pages in the nav-bar, the z-index change
-
-// this can maybe be written better, in a loop or if/else or both
+// this can be written better, in a loop. (to do when have time)
 
 aboutBtn.addEventListener("click", () => {
   about.style.zIndex = "6";
@@ -48,10 +40,10 @@ projectsBtn.addEventListener("click", () => {
 
 const projectsData = [
   {
-    image: "project-meal-big.png",
+    image: "meal-review-big.png",
     name: "Meal review",
     description:
-      "Ever gone to a new resturant to try it out? Got some take-away from some new place? Two months later you can't remeber if you liked it or what you even ate. Keep track of your own culenary experiences using this personal meal rating app.",
+      "Have you ever gone to a new resturant to try it out? Got some take-away from some new place? Two months later you can't remeber if you liked it or what you even ate. Keep track of your own culenary experiences using this personal meal rating app.",
     tools: ["html.png", "css.png", "js.png"],
     links: [
       {
@@ -68,7 +60,7 @@ const projectsData = [
     image: "project-drum-big.png",
     name: "Drum machine",
     description:
-      "A simple drumkit that allows you to bring out your music creativity",
+      "A simple drumkit that allows you to bring out your musical creativity. Play using keyboard letters or click with the mouse. Don't stop until the neighbours complain 😎 ",
     tools: ["html.png", "css.png", "js.png"],
     links: [
       {
@@ -84,7 +76,7 @@ const projectsData = [
   {
     image: "project-quiz-big.png",
     name: "Quiz game",
-    description: "Test your general knowledge in this little quiz game.",
+    description: "Test your general knowledge in this little quiz game. Can you get all 10 answers right? Score function will be added in a future update",
     tools: ["html.png", "css.png", "js.png"],
     links: [
       {
@@ -98,88 +90,76 @@ const projectsData = [
     ],
   },
   {
-    image: "project-meal-big.png",
-    name: "Meal review",
+    image: "project-match-big.png",
+    name: "Match two game",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
+      "This match two game would offer plenty of fun with a large pool of randomized images to match up. Too bad it's incomplete and full of bugs that there has been no time to finish, fix and polish. Maybe one day this will be complete 😁",
     tools: ["html.png", "css.png", "js.png"],
     links: [
       {
         logo: "github2.png",
-        url: "https://github.com/Kodehode-Stavanger/user-data-assignment-Veronica-Kodehode",
+        url: "https://github.com/Kodehode-Stavanger/javascript-basic-mini-project-Veronica-Kodehode",
       },
       {
         logo: "go.png",
-        url: "https://kodehode-stavanger.github.io/user-data-assignment-Veronica-Kodehode/",
+        url: "https://kodehode-stavanger.github.io/javascript-basic-mini-project-Veronica-Kodehode/",
       },
     ],
   },
   {
-    image: "project-meal-big.png",
-    name: "Meal review",
+    image: "project-topten-big.png",
+    name: "The 10 best selling video games of all time",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
+      "A project assignment to create a webpage called the 10 best selling video games of all time, listing,... you guessed it... the 10 best selling video games of all time.. 😉",
     tools: ["html.png", "css.png", "js.png"],
     links: [
       {
         logo: "github2.png",
-        url: "https://github.com/Kodehode-Stavanger/user-data-assignment-Veronica-Kodehode",
+        url: "https://github.com/Veronica-Kodehode/top-10-games",
       },
       {
         logo: "go.png",
-        url: "https://kodehode-stavanger.github.io/user-data-assignment-Veronica-Kodehode/",
+        url: "https://veronica-kodehode.github.io/top-10-games/",
       },
     ],
   },
   {
-    image: "project-meal-big.png",
-    name: "Meal review",
+    image: "project-AI-big.png",
+    name: "Fake AI homebot store",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
-    tools: ["html.png", "css.png", "js.png"],
+      "A figma project of our choice where I am making a fake store selling AI bots for your everyday needs. This is a project focused on creativity and design while utilizing good figma practices. This project is still in the works.",
+    tools: ["figma.png"],
     links: [
       {
-        logo: "github2.png",
-        url: "https://github.com/Kodehode-Stavanger/user-data-assignment-Veronica-Kodehode",
-      },
-      {
-        logo: "go.png",
-        url: "https://kodehode-stavanger.github.io/user-data-assignment-Veronica-Kodehode/",
+        logo: "figma2.png",
+        url: "https://www.figma.com/file/KTUm5Wm0jYnox50K3bVTxR/My-webshop?type=design&node-id=0%3A1&mode=design&t=1UQ0mBCCDULHesUJ-1",
       },
     ],
   },
   {
-    image: "project-meal-big.png",
-    name: "Meal review",
+    image: "project-planet-big.png",
+    name: "The planet Jupiter",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
-    tools: ["html.png", "css.png", "js.png"],
+      "A very simple planet project. As an early assignment we created a simple website displaying the planet Jupiter and all the glorious facts about the giant planet.",
+    tools: ["html.png", "css.png"],
     links: [
       {
         logo: "github2.png",
-        url: "https://github.com/Kodehode-Stavanger/user-data-assignment-Veronica-Kodehode",
+        url: "https://github.com/elliot-kodehode/assignment-3",
       },
       {
         logo: "go.png",
-        url: "https://kodehode-stavanger.github.io/user-data-assignment-Veronica-Kodehode/",
+        url: "https://elliot-kodehode.github.io/assignment-3/",
       },
     ],
   },
   {
-    image: "project-meal-big.png",
-    name: "Meal review",
+    image: "project-creativity-big.png",
+    name: "To be done",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
-    tools: ["html.png", "css.png", "js.png"],
+      "More fun projects coming soon to a device near you 🤩",
+    tools: ["imagination.png"],
     links: [
-      {
-        logo: "github2.png",
-        url: "https://github.com/Kodehode-Stavanger/user-data-assignment-Veronica-Kodehode",
-      },
-      {
-        logo: "go.png",
-        url: "https://kodehode-stavanger.github.io/user-data-assignment-Veronica-Kodehode/",
-      },
     ],
   },
 ];
@@ -188,14 +168,15 @@ const projectsData = [
 
 function displayProjectFunction(index) {
   creativity.style.visibility = "hidden";
-
+  
   const project = projectsData[index];
-  const projectContainer = document.getElementById("project-container");
+ 
   // Clear previous content
   while (projectContainer.firstChild) {
-    projectContainer.removeChild(projectContainer.firstChild);
+  projectContainer.removeChild(projectContainer.firstChild);
   }
 
+  // create container for big project image and get image out of object array and place in container
   const imageContainer = document.createElement("div");
   imageContainer.classList.add("image-container");
   projectContainer.append(imageContainer);
@@ -203,12 +184,11 @@ function displayProjectFunction(index) {
   const image = document.createElement("img");
   image.src = `images/${project.image}`;
   image.classList.add("projects-img");
-
   imageContainer.append(image);
 
+  // create container for big project text and get data out of object array and place in their containers
   const textContainer = document.createElement("div");
   textContainer.classList.add("text-container");
-
   projectContainer.append(textContainer);
 
   const upperContainer = document.createElement("div");
@@ -223,7 +203,6 @@ function displayProjectFunction(index) {
   title.classList.add("title");
   const titleText = document.createElement("p");
   titleText.textContent = project.name;
-
   title.appendChild(titleText);
 
   upperContainer.appendChild(title);
@@ -235,32 +214,26 @@ function displayProjectFunction(index) {
 
   const tools = document.createElement("div");
   tools.classList.add("tools");
-  // tools.textContent = "Tools used"
   project.tools.forEach((tool) => {
-    const toolImg = document.createElement("img");
-    toolImg.src = `images/${tool}`;
-    toolImg.classList.add("tool-img");
-    tools.appendChild(toolImg);
+  const toolImg = document.createElement("img");
+  toolImg.src = `images/${tool}`;
+  toolImg.classList.add("tool-img");
+  tools.appendChild(toolImg);
   });
-  upperContainer.appendChild(tools);
+   upperContainer.appendChild(tools);
 
   const links = document.createElement("div");
   links.classList.add("links");
   project.links.forEach((link) => {
-    const linkAnchor = document.createElement("a");
-    linkAnchor.href = link.url;
+  const linkAnchor = document.createElement("a");
+  linkAnchor.href = link.url;
 
-    const linkImg = document.createElement("img");
-    linkImg.src = `images/${link.logo}`;
-    linkImg.classList.add("link-img");
+  const linkImg = document.createElement("img");
+  linkImg.src = `images/${link.logo}`;
+  linkImg.classList.add("link-img");
 
-    linkAnchor.appendChild(linkImg);
-    links.appendChild(linkAnchor);
+  linkAnchor.appendChild(linkImg);
+  links.appendChild(linkAnchor);
   });
   bottomContainer.appendChild(links);
-}
-
-// Function to show specific project info
-function showInfo(index) {
-  const project = projectsData[index];
 }
