f1820f613cb478ac7d57f29a4b00e5cd18910e66
Veronica Kodehode <Veronicas.kodehode@gmail.com>
2024-03-11 13:43:54 +0100

    working hard

--- script.js
+++ script.js
@@ -9,44 +9,40 @@ const displayProjects = document.getElementById("display-projects");
 const navBar = document.getElementById("nav-bar");
 const projectOne = document.getElementById("project-one");
 const projectContainer = document.getElementById("project-container");
-const contactBtn = document.getElementById("contact-btn")
-const creativity = document.getElementById("creativity")
-const navBtn = document.getElementById("nav-btn")
+const contactBtn = document.getElementById("contact-btn");
+const creativity = document.getElementById("creativity");
+const navBtn = document.getElementById("nav-btn");
 
 // when clicking between the pages in the nav-bar, the z-index change
 
 // this can maybe be written better, in a loop or if/else or both
 
-
-
 aboutBtn.addEventListener("click", () => {
   about.style.zIndex = "6";
   home.style.zIndex = "0";
   projects.style.zIndex = "0";
-  aboutBtn.style.opacity = "1"
-  homeBtn.style.opacity = "0.5"
-  projectsBtn.style.opacity = ".5"
+  aboutBtn.style.opacity = "1";
+  homeBtn.style.opacity = "0.5";
+  projectsBtn.style.opacity = ".5";
 });
 
 homeBtn.addEventListener("click", () => {
   home.style.zIndex = "6";
   projects.style.zIndex = "0";
   about.style.zIndex = "0";
-  homeBtn.style.opacity = "1"
-  projectsBtn.style.opacity = ".5"
-  aboutBtn.style.opacity = ".5"
+  homeBtn.style.opacity = "1";
+  projectsBtn.style.opacity = ".5";
+  aboutBtn.style.opacity = ".5";
 });
 
 projectsBtn.addEventListener("click", () => {
   projects.style.zIndex = "6";
   about.style.zIndex = "0";
   home.style.zIndex = "0";
-  projectsBtn.style.opacity = "1"
-  homeBtn.style.opacity = "0.5"
-  aboutBtn.style.opacity = ".5"
+  projectsBtn.style.opacity = "1";
+  homeBtn.style.opacity = "0.5";
+  aboutBtn.style.opacity = ".5";
 });
-
-
 
 // object-array of projects data
 
@@ -55,74 +51,143 @@ const projectsData = [
     image: "project-meal-big.png",
     name: "Meal review",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
+      "Ever gone to a new resturant to try it out? Got some take-away from some new place? Two months later you can't remeber if you liked it or what you even ate. Keep track of your own culenary experiences using this personal meal rating app.",
     tools: ["html.png", "css.png", "js.png"],
-    links: [{logo: "github2.png", url: "https://github.com/Kodehode-Stavanger/user-data-assignment-Veronica-Kodehode" }, {logo: "go.png", url: "https://kodehode-stavanger.github.io/user-data-assignment-Veronica-Kodehode/"}],
+    links: [
+      {
+        logo: "github2.png",
+        url: "https://github.com/Kodehode-Stavanger/user-data-assignment-Veronica-Kodehode",
+      },
+      {
+        logo: "go.png",
+        url: "https://kodehode-stavanger.github.io/user-data-assignment-Veronica-Kodehode/",
+      },
+    ],
   },
   {
     image: "project-drum-big.png",
-    name: "Meal review",
+    name: "Drum machine",
     description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
+      "A simple drumkit that allows you to bring out your music creativity",
     tools: ["html.png", "css.png", "js.png"],
-    links: ["github2.png", "go.png"],
+    links: [
+      {
+        logo: "github2.png",
+        url: "https://github.com/Kodehode-Stavanger/javascript-advanced-project-drum-machine-Veronica-Kodehode",
+      },
+      {
+        logo: "go.png",
+        url: "https://kodehode-stavanger.github.io/javascript-advanced-project-drum-machine-Veronica-Kodehode/",
+      },
+    ],
   },
   {
-    image: "project-match-big.png",
-    name: "Meal review",
-    description:
-      "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
+    image: "project-quiz-big.png",
+    name: "Quiz game",
+    description: "Test your general knowledge in this little quiz game.",
     tools: ["html.png", "css.png", "js.png"],
-    links: ["github2.png", "go.png"],
+    links: [
+      {
+        logo: "github2.png",
+        url: "https://github.com/Kodehode-Stavanger/javascript-advanced-project-api-Veronica-Kodehode",
+      },
+      {
+        logo: "go.png",
+        url: "https://kodehode-stavanger.github.io/javascript-advanced-project-api-Veronica-Kodehode/",
+      },
+    ],
   },
   {
-    image: "meal-review2.png",
+    image: "project-meal-big.png",
     name: "Meal review",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
     tools: ["html.png", "css.png", "js.png"],
-    links: ["github2.png", "go.png"],
+    links: [
+      {
+        logo: "github2.png",
+        url: "https://github.com/Kodehode-Stavanger/user-data-assignment-Veronica-Kodehode",
+      },
+      {
+        logo: "go.png",
+        url: "https://kodehode-stavanger.github.io/user-data-assignment-Veronica-Kodehode/",
+      },
+    ],
   },
   {
-    image: "meal-review2.png",
+    image: "project-meal-big.png",
     name: "Meal review",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
     tools: ["html.png", "css.png", "js.png"],
-    links: ["github2.png", "go.png"],
+    links: [
+      {
+        logo: "github2.png",
+        url: "https://github.com/Kodehode-Stavanger/user-data-assignment-Veronica-Kodehode",
+      },
+      {
+        logo: "go.png",
+        url: "https://kodehode-stavanger.github.io/user-data-assignment-Veronica-Kodehode/",
+      },
+    ],
   },
   {
-    image: "meal-review2.png",
+    image: "project-meal-big.png",
     name: "Meal review",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
     tools: ["html.png", "css.png", "js.png"],
-    links: ["github2.png", "go.png"],
+    links: [
+      {
+        logo: "github2.png",
+        url: "https://github.com/Kodehode-Stavanger/user-data-assignment-Veronica-Kodehode",
+      },
+      {
+        logo: "go.png",
+        url: "https://kodehode-stavanger.github.io/user-data-assignment-Veronica-Kodehode/",
+      },
+    ],
   },
   {
-    image: "meal-review2.png",
+    image: "project-meal-big.png",
     name: "Meal review",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
     tools: ["html.png", "css.png", "js.png"],
-    links: ["github2.png", "go.png"],
+    links: [
+      {
+        logo: "github2.png",
+        url: "https://github.com/Kodehode-Stavanger/user-data-assignment-Veronica-Kodehode",
+      },
+      {
+        logo: "go.png",
+        url: "https://kodehode-stavanger.github.io/user-data-assignment-Veronica-Kodehode/",
+      },
+    ],
   },
   {
-    image: "meal-review2.png",
+    image: "project-meal-big.png",
     name: "Meal review",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio id dolorum vel nostrum? Officiis numquam officia dolor eaque? Mollitia quae eaque atque recusandae. Explicabo neque quaerat et. Repellat repudiandae labore vitae laboriosam ea molestias atque alias unde?",
     tools: ["html.png", "css.png", "js.png"],
-    links: ["github2.png", "go.png"],
+    links: [
+      {
+        logo: "github2.png",
+        url: "https://github.com/Kodehode-Stavanger/user-data-assignment-Veronica-Kodehode",
+      },
+      {
+        logo: "go.png",
+        url: "https://kodehode-stavanger.github.io/user-data-assignment-Veronica-Kodehode/",
+      },
+    ],
   },
 ];
-
 
 // main function to display big info-box on projects
 
 function displayProjectFunction(index) {
-
-creativity.style.visibility = "hidden"
+  creativity.style.visibility = "hidden";
 
   const project = projectsData[index];
   const projectContainer = document.getElementById("project-container");
@@ -131,9 +196,15 @@ creativity.style.visibility = "hidden"
     projectContainer.removeChild(projectContainer.firstChild);
   }
 
+  const imageContainer = document.createElement("div");
+  imageContainer.classList.add("image-container");
+  projectContainer.append(imageContainer);
+
   const image = document.createElement("img");
   image.src = `images/${project.image}`;
   image.classList.add("projects-img");
+
+  imageContainer.append(image);
 
   const upperContainer = document.createElement("div");
   upperContainer.classList.add("upper-container");
@@ -141,7 +212,7 @@ creativity.style.visibility = "hidden"
   const bottomContainer = document.createElement("div");
   bottomContainer.classList.add("bottom-container");
 
-  projectContainer.append(image, upperContainer, bottomContainer);
+  projectContainer.append(upperContainer, bottomContainer);
 
   const title = document.createElement("div");
   title.classList.add("title");
@@ -171,20 +242,18 @@ creativity.style.visibility = "hidden"
   const links = document.createElement("div");
   links.classList.add("links");
   project.links.forEach((link) => {
-    
     const linkAnchor = document.createElement("a");
     linkAnchor.href = link.url;
-    
+
     const linkImg = document.createElement("img");
     linkImg.src = `images/${link.logo}`;
     linkImg.classList.add("link-img");
 
     linkAnchor.appendChild(linkImg);
     links.appendChild(linkAnchor);
-
   });
   bottomContainer.appendChild(links);
-  }
+}
 
 // Function to show specific project info
 function showInfo(index) {
